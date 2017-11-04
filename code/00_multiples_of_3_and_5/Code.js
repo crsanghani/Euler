4eeb10ba45f13402d5015128e94b7beb937edbf4
crsanghani <crsanghani@gmail.com>
2017-11-04 16:47:20 +0000

    finished tests for identifying and storing multiples

--- code/00_multiples_of_3_and_5/Code.js
+++ code/00_multiples_of_3_and_5/Code.js
@@ -1,10 +1,31 @@
 function Code() {
+    multiplesOfThree: [];
+    multiplesOfFive: [];
+    multiplesOfFifteen: [];
 }
 
 Code.prototype.multiples = function(int) {
+    let storedMultiplesOfThree = [];
+    let storedMultiplesOfFive = [];
+    let storedMultiplesOfFifteen = [];
+
     if (int < 3) {
         return 'No multiples found.';
     }
+
+    for (let i = 1; i < int + 1; i++) {
+        if (i % 15 === 0) {
+            storedMultiplesOfFifteen.push(i);
+        } else if (i % 5 === 0) {
+            storedMultiplesOfFive.push(i);
+        } else if (i % 3 === 0) {
+            storedMultiplesOfThree.push(i);
+        }
+    }
+
+    this.multiplesOfThree = storedMultiplesOfThree;
+    this.multiplesOfFive = storedMultiplesOfFive;
+    this.multiplesOfFifteen = storedMultiplesOfFifteen;
 };
 
 module.exports = Code;
--- spec/00Spec.js
+++ spec/00Spec.js
@@ -7,12 +7,24 @@ describe('Sum of the multiples of 3 and 5 below 100', () => {
     });
 
     describe('Finding the multiples of 3 and 5', () => {
-        // it('should store all the multiples of 3 in an array', () => {
-        //     expect(code.multiples)
-        // });
 
         it('should return "No multiples" if arg is < 3 ', () => {
             expect(code.multiples(2)).toBe('No multiples found.');
+        });
+
+        it('should store multiples of 3 in an array', () => {
+            code.multiples(6);
+            expect(code.multiplesOfThree).toContain(3, 6);
+        });
+
+        it('should store multiples of 5 in an array', () => {
+            code.multiples(10);
+            expect(code.multiplesOfFive).toContain(5, 10);
+        });
+
+        it('should store multiples of 15 in an array', () => {
+            code.multiples(15);
+            expect(code.multiplesOfFifteen).toContain(15);
         });
     });
 });
